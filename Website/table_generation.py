537807a017facbeaf57b4c244c6a1d0456f05eba
Sandeep Nuthalapati <deepsudha@gmail.com>
2023-09-11 23:29:07 +1000

    refactor table generation code

--- Website/table_generation.py
+++ Website/table_generation.py
@@ -5,36 +5,43 @@ Created on Sat Sep 22 14:49:00 2018
 Modified on Fri Dec 07 23:18:00 2018
 @author: scottveirs
 """
+from pathlib import Path
+from jinja2 import Environment, FileSystemLoader
 
-from os import listdir
-from os.path import isfile, join
 
-#Please update this to YOUR path to orcadata
-#basePath = r"C:\Users\kmuss\Desktop"
-basePath = r"/Users/scott/Dropbox/Scottcoding/Orcasound/"
+def get_folder_path(folder: list) -> Path:
+    return Path.cwd().joinpath(*folder)
 
-#plotPath = join(basePath, "orcadata", "spectrogram", "Plots")
-folderpath_flac = join(basePath, "orcadata", "Sounds", "catalog", "flac")
-onlyfiles = sorted([f for f in listdir(folderpath_flac) if isfile(join(join(folderpath_flac, f)))])
-htmlpath = join(basePath, "orcadata", "Website")
-base_URL = "http://www.orcasound.net/data/product/SRKW/call-catalog/no-narration_flac+mp3+ogg+spectrograms/"
+
+sounds_path = get_folder_path(["Sounds", "catalog", "flac"])
+flac_files = [f for f in sounds_path.iterdir() if f.is_file()]
+
+htmlpath = get_folder_path(["Website"])
+base_URL = "http://www.orcasound.net/data/product/SRKW/call-catalog/no-narration_flac+mp3+ogg+spectrograms"
 title_text = "Sound spectrum table"
 
-f = open(join(htmlpath, "spectrum_comparison.html"), "w")
-f.write("<html>\n\n<head>\n<title>" + title_text + "</title>\n</head>\n\n<body>\n<h2>" + title_text + "</h2>\n ")
-f.write("<table>\n")
-for file in onlyfiles:
-    file_name = file.split(".")[0]
-    heading = file_name.split('-')[1]
-    f.write("<tr>\n<td>\n")
-    f.write("<h3>" + heading + "<h3>\n")
-    f.write("<audio controls>\n")
-    f.write("<source src=\"" + base_URL + "mp3/" + file_name + ".mp3\" " 'type="audio/mp3">\n')
-    f.write("<source src=\"" + base_URL + "ogg/" + file_name + ".ogg\" " 'type="audio/ogg">\n')
-    f.write("</audio>\n")
-    f.write("</td>\n")
-    f.write("<td> <img src=\"" + base_URL + "spectrogram-pngs/" + file_name + ".png\"> </td>\n")
-    f.write("<td> <img src=\"" + "http://www.orcasound.net/data/raw/SRKW-Ford-Osborne-tapes/Ford89-SRKW-spectrograms/" + heading + ".png\"> </td>\n")
-    f.write("</tr>\n")
-f.write("</table>\n</body>\n</html>")
+env = Environment(loader=FileSystemLoader(htmlpath))
+template = env.get_template("spectrum_template.html")
+data = []
+
+for file in flac_files:
+    file_name = file.stem
+    heading = file_name.split("-")[1]
+
+    data.append({
+        'heading': heading,
+        'mp3_url': "/".join([base_URL, "mp3", f"{file_name}.mp3"]),
+        'ogg_url': "/".join([base_URL, "ogg", f"{file_name}.ogg"]),
+        'spectrogram_url': "/".join([base_URL, "spectrogram-pngs", f"{file_name}.png"]),
+        'raw_spectrogram_url': f"http://www.orcasound.net/data/raw/SRKW-Ford-Osborne-tapes/Ford89-SRKW-spectrograms/{heading}.png"
+    })
+
+final_html = template.render(**{
+    'title': title_text,
+    'data': data
+})
+
+with open(htmlpath.joinpath("spectrum_comparison.html"), "w") as fh:
+    fh.write(final_html)
+
 
